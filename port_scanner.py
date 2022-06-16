d3a11beaf3b38df5da01141f6fdc22a11b6cf91d
AbhinavGunti <78372006+AbhinavGunti@users.noreply.github.com>
2022-06-16 20:14:42 +0530

    Resolved Port Bug

--- port_scanner.py
+++ port_scanner.py
@@ -1,78 +1,55 @@
-txt = "Industry Problem 1 : Computer Networks Assignment"
-x = txt.center(460)
-print(x)
 import socket
 import ipaddress   
 import re
-port_range_pattern = re.compile("([0-9]+)-([0-9]+)")
+import requests
+print("Industry Problem 1 : Computer Networks Assignment")
+
 port_min = 0
 port_max = 65535
 open_ports = []
+
 while True:
     ip_add_entered = input("\nPlease enter the ip address that you want to scan: ")
     try:
         ip_address_obj = ipaddress.ip_address(ip_add_entered)
-        print("You entered a valid ip address.")
+        print("VALID IP ADDRESS.")
         break
     except:
-        print("You entered an invalid ip address, please try again!!!")
-import requests
+        print("INVALID IP ADDRESS!")
+
 print("\n")
+
 print("Location details of the entered ip address are : \n")
 response = requests.post("http://ip-api.com/batch", json=[
   {"query": ip_add_entered}
 ]).json()
 for ip_info in response:
     for k,v in ip_info.items():
-        print(k,v)
+        print(f"{k} : {v}")
     print("\n")
-while True:
-    print("Please enter the range of ports you want to scan in format: <int>-<int> (ex would be 60-120)")
-    port_range = input("Enter port range: ")
-    port_range_valid = port_range_pattern.search(port_range.replace(" ",""))
-    if port_range_valid:
-        port_min = int(port_range_valid.group(1))
-        port_max = int(port_range_valid.group(2))
-        break
-for port in range(port_min, port_max + 1):
-    try:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.settimeout(0.5)
-            s.connect((ip_add_entered, port))
-            open_ports.append(port)
-    except:
-        pass
-for port in open_ports:
-    print(f"Port {port} is open on {ip_add_entered}.") 
-import ipaddress
-import re
 port_range_pattern = re.compile("([0-9]+)-([0-9]+)")
-port_min = 0
-port_max = 65535
-open_ports = []
+
 while True:
-    ip_add_entered = input("\nPlease enter the ip address that you want to scan: ")
-    try:
-        ip_address_obj = ipaddress.ip_address(ip_add_entered)
-        print("You entered a valid ip address.")
-        break
-    except:
-        print("You entered an invalid ip address")
-while True:
-    print("Please enter the range of ports you want to scan in format: <int>-<int> (ex would be 60-120)")
+    print("Please enter the range of ports you want to scan in format: <int>-<int>")
     port_range = input("Enter port range: ")
     port_range_valid = port_range_pattern.search(port_range.replace(" ",""))
+    print(port_range_valid,port_range_valid.group(1),port_range_valid.group(2))
     if port_range_valid:
         port_min = int(port_range_valid.group(1))
         port_max = int(port_range_valid.group(2))
         break
+    
 for port in range(port_min, port_max + 1):
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.settimeout(0.5)
+            s.settimeout(1)
             s.connect((ip_add_entered, port))
             open_ports.append(port)
-    except:
-        pass
-for port in open_ports:
-    print(f"Port {port} is open on {ip_add_entered}.")
+    except :
+        # pass
+        print(f"Port {port} not open on {ip_add_entered}\t Open Ports : {open_ports}")
+if len(open_ports)!=0:
+    for port in open_ports:
+        print(f"Port {port} is open on {ip_add_entered}.") 
+else:
+    print(f"No open ports from {port_min}-{port_max}")
